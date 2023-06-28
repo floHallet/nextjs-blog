7669fb2b24696769737d142a5d4d341b06b39c68
floHal <florent.hallet@inetum.com>
2023-06-28 15:34:43 +0200

    API

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,7 +1,3 @@
-import { kv } from '@vercel/kv';
- 
-export default async function handler(request, response) {
-  const counter = await kv.get('counter:portfolio');
-  await kv.incr('counter:portfolio');
-  return response.status(200).send(counter);
-}
+export default function handler(req, res) {
+    res.status(200).json({ text: 'Hello' });
+  }
