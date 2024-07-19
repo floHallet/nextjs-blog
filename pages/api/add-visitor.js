564765f369c4f52bc7105139662bb5c81f703d09
floHal <florent.hallet@inetum.com>
2024-07-19 14:25:07 +0200

    TRY another table approach

--- pages/api/add-visitor.js
+++ pages/api/add-visitor.js
@@ -1,17 +1,20 @@
 import { sql } from '@vercel/postgres';
- 
+
 export default async function handler(request, response) {
   try {
-    //const id = Math.floor(Math.random()*1000).toString();
+    //Get IP Address
     const forwarded = request.headers["x-forwarded-for"];
-    const id = forwarded ? forwarded.split(/, /)[0] : req.connection.remoteAddress;
+    const ip = forwarded ? forwarded.split(/, /)[0] : req.connection.remoteAddress;
+    //Get Page
     const page = request.query.page || "testing";
-    const visit = new Date().toISOString();
-    await sql`INSERT INTO Visitors (Id, Page, Visit) VALUES (${id}, ${page}, ${visit});`;
+    //Get actual time
+    const visit_date = new Date().toISOString();
+
+    await sql`INSERT INTO Visitors (Visit_Date, Page, Ip) VALUES (, ${visit_date}, ${page}, ${ip});`;
   } catch (error) {
     return response.status(500).json({ error });
   }
- 
+
   //const visitors = await sql`SELECT * FROM Visitors;`;
-  return response.status(200).json('Success!');
+  return response.status(200).json('Visitor added with success!');
 }
