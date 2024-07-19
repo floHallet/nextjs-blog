909cb31dc839b761aa7707de67aa748104cb6bf4
floHal <florent.hallet@inetum.com>
2024-07-19 12:08:18 +0200

    GET user ip

--- pages/api/add-visitor.js
+++ pages/api/add-visitor.js
@@ -2,17 +2,16 @@ import { sql } from '@vercel/postgres';
  
 export default async function handler(request, response) {
   try {
-    //const petName = request.query.petName;
-    //const ownerName = request.query.ownerName;
-    //if (!petName || !ownerName) throw new Error('Pet and owner names required');
-    const id = Math.floor(Math.random()*1000).toString();
+    //const id = Math.floor(Math.random()*1000).toString();
+    const forwarded = request.headers["x-forwarded-for"];
+    const id = forwarded ? forwarded.split(/, /)[0] : req.connection.remoteAddress;
     const page = "testing";
-    const visit = new Date().toISOString();
+    const visit = new Date();
     await sql`INSERT INTO Visitors (Id, Page, Visit) VALUES (${id}, ${page}, ${visit});`;
   } catch (error) {
     return response.status(500).json({ error });
   }
  
-  const visitors = await sql`SELECT * FROM Visitors;`;
-  return response.status(200).json({ visitors });
+  //const visitors = await sql`SELECT * FROM Visitors;`;
+  return response.status(200).json('Success!');
 }
