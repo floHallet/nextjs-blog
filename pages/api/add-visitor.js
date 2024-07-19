34726debf31990fecd4e1ad37977d55a157b140e
floHal <florent.hallet@inetum.com>
2024-07-19 12:17:03 +0200

    CHANGE table visitors parameters

--- pages/api/add-visitor.js
+++ pages/api/add-visitor.js
@@ -5,8 +5,8 @@ export default async function handler(request, response) {
     //const id = Math.floor(Math.random()*1000).toString();
     const forwarded = request.headers["x-forwarded-for"];
     const id = forwarded ? forwarded.split(/, /)[0] : req.connection.remoteAddress;
-    const page = "testing";
-    const visit = new Date();
+    const page = request.query.page || "testing";
+    const visit = new Date().toISOString();
     await sql`INSERT INTO Visitors (Id, Page, Visit) VALUES (${id}, ${page}, ${visit});`;
   } catch (error) {
     return response.status(500).json({ error });
--- pages/api/create-visitors.js
+++ pages/api/create-visitors.js
@@ -3,7 +3,7 @@ import { sql } from '@vercel/postgres';
 export default async function handler(request, response) {
   try {
     const result =
-      await sql`CREATE TABLE Visitors ( Id varchar(100), Page varchar(100), Visit date);`;
+      await sql`CREATE TABLE Visitors ( Id varchar(50), Page varchar(50), Visit timestampz);`;
     return response.status(200).json({ result });
   } catch (error) {
     return response.status(500).json({ error });
