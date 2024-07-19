493e3922d4028a8edb18a5680b25a465a30850e5
floHal <florent.hallet@inetum.com>
2024-07-19 14:45:12 +0200

    add missing declaration

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -3,6 +3,7 @@ import { getAllPostIds, getPostData } from "../../lib/posts";
 import Head from "next/head";
 import Date from '../../components/date';
 import utilStyles from '../../styles/utils.module.css';
+import { useEffect } from 'react';
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id);
